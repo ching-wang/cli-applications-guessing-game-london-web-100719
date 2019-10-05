e9445e806afcca55d470363ba6bff5adacdeb67a
Qing Wang <chingwang.qw@gmail.com>
2019-10-05 19:53:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,9 +4,9 @@ def run_guessing_game()
   guess_result = gets.chomp 
   if guess_result == random_number
      puts "You guessed the correct number!"
-  elsif  
-     puts "Sorry! The computer guessed #{random_number}."
   elsif guess_result == "exit"
      puts "Goodbye!"
-   end 
+  else
+   puts "Sorry! The computer guessed #{random_number}."
+ end 
 end 
