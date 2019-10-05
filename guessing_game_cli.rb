793460b3ec6223f19bac8042575314e133339454
Qing Wang <chingwang.qw@gmail.com>
2019-10-05 19:35:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game()
   ramdom_number = rand(1..7)
   puts "Guess a number between 1 to 6"
   guess_result = gets.chomp 
-  if guess_result == ramdom_number
+  if guess_result == ramdom_number.to_i
      puts "Your guessed the correct number!"
     else 
      puts "Sorry! The computer guessed #{ramdom_number}"
