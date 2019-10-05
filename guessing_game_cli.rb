7848a18087c92164bfd1a41edaf28b9b3f74ac25
Qing Wang <chingwang.qw@gmail.com>
2019-10-05 20:00:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game()
   random_number = rand(6) + 1
   # puts "Guess a number between 1 to 6."
-  guess_result = gets.to_i
+  guess_result = gets.chomp.to_i
   if guess_result == random_number.to_i
      puts "You guessed the correct number!"
   elsif guess_result == "exit"
